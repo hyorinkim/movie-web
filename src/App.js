1a875324b83cba2deb9775b50053a44207feb95c
hyorinkim <gyfls7748@gmail.com>
2022-01-11 23:31:42 +0900

    state가 변화될때 마다  rerender 문제 있음

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Button from "./Button";
 import styles from './App.module.css'
+import { useState} from 'react'
 function App(){
+    const [counter,setValue] = useState(0)
+
+    const Click=()=>{
+        console.log('call an api')
+        setValue((prev)=>prev+1)
+    }
     return (
         <div>
-            <h1 className={styles.title}>Welcome back!</h1>
+            <h1 className={styles.title}>{counter}</h1>
+            <button onClick={Click}>Click me</button>
             <Button text={'continue'}/>
         </div>
     )
