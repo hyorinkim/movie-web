f2e5cf7cf793457490fab1511376661796787dae
hyorinkim <gyfls7748@gmail.com>
2022-01-20 16:16:32 +0900

    cleanup function 정의

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Button from "./Button";
 import styles from './App.module.css'
 import { useState , useEffect} from 'react'
+function Hello(){
+    useEffect(()=>{
+        console.log("created");
+        return () => console.log("destoryed")//cleanup function 컴포넌트가 destroy될 때 뭔가 할 수 있다.
+    },[])
+    return <h1> Hello</h1>
+}
 function App(){
+    const [showing,setShowing] = useState(false)
     const [counter,setValue] = useState(0)
     const [keyword,setKeyword]=useState("")
+
+    const onClick=()=> setShowing((prev)=>!prev)
     const Click=()=> setValue((prev)=>prev+1)
     const onChange=(event)=> setKeyword(event.target.value)
+
     console.log("i run all the time");
     useEffect(()=>{
         console.log("I run only once.")//컴포넌트 내에서 몇몇 코드는 처음에만 render하고 rerender 안하도록 원할 수 있다.
@@ -21,8 +32,13 @@ function App(){
     useEffect(()=>{
         console.log("I run when keywword &counter changes")        
     },[keyword,counter]);//keyword나 counter 중에 하나라도 바뀌면 호출
+    
     return (
         <div>
+           {showing ? <Hello/>:null}
+           <button onClick={onClick}>{showing?"Hide":"Show"}</button>
+            
+            <br></br>
             <input 
             value={keyword}
             onChange={onChange}
